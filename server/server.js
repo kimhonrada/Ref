4d758d0b1d2415ce69fb5d245402c015435ed970
Kim Honrada <kimhonrada21@gmail.com>
2021-12-02 19:36:44 -0500

    Establish initial get endpoint in server

--- server/server.js
+++ server/server.js
@@ -5,3 +5,10 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Listening at port ${port}`);
 });
+
+app.use(express.json());
+
+// get food
+app.get("/food", (req, res) => {
+  res.send("this is your food endpoint");
+});
