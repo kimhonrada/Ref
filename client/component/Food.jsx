35639bd3e638b73d062bb2e7a07cadd78b1a15f0
Kim Honrada <kimhonrada21@gmail.com>
2021-12-02 21:29:59 -0500

    Render food images inside the fridge

--- client/component/Food.jsx
+++ client/component/Food.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Food = (props) => {
 
   const foodBlocks = props.food.map((food) =>
-    <div key={food.id} className="food">{food.name}</div>
+    // <div key={food.id} className="food"><img src={food.url} alt={food.name}></img></div>
+    <img className="food" src={food.picture} alt={food.name} key={food.id}></img>
   )
 
   return (
